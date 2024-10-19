57595c3caa33fb5c2f3048f5a30e9c94e1cce079
vexstudentsteamlabs <student@steamlabs.co.th>
2024-10-19 16:28:15 +0700

    thick of it - ksi & trippie red

--- src/main.py
+++ src/main.py
@@ -8,6 +8,62 @@
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
+"""I'm in the thick of it, everybody knows
+They know me where it snows, I skied in and they froze
+I don't know no nothin' 'bout no ice, I'm just cold
+Forty somethin' milli' subs or so, I've been told
+
+I'm in my prime, and this ain't even final form
+They knocked me down, but still, my feet, they find the floor
+I went from living rooms straight out to sold-out tours
+Life's a fight, but trust, I'm ready for the war
+
+Woah-oh-oh
+This is how the story goes
+Woah-oh-oh
+I guess this is how the story goes
+
+I'm in the thick of it, everybody knows
+They know me where it snows, I skied in and they froze
+I don't know no nothin' 'bout no ice, I'm just cold
+Forty somethin' milli' subs or so, I've been told
+
+From the screen to the ring, to the pen, to the king
+Where's my crown? That's my bling, always drama when I ring
+See, I believe that if I see it in my heart
+Smash through the ceiling 'cause I'm reaching for the stars
+
+Woah-oh-oh
+This is how the story goes
+Woah-oh-oh
+I guess this is how the story goes
+
+I'm in the thick of it, everybody knows
+They know me where it snows, I skied in and they froze (Woo)
+I don't know no nothin' 'bout no ice, I'm just cold
+Forty somethin' milli' subs or so, I've been told
+
+Highway to heaven, I'm just cruisin' by my lone'
+They cast me out, left me for dead, them people cold
+My faith in God, mind in the sun, I'm 'bout to sow (Yeah)
+My life is hard, I took the wheel, I cracked the code (Yeah-yeah, woah-oh-oh)
+Ain't nobody gon' save you, man, this life will break you (Yeah, woah-oh-oh)
+In the thick of it, this is how the story goes
+
+I'm in the thick of it, everybody knows
+They know me where it snows, I skied in and they froze (Woo)
+I don't know no nothin' 'bout no ice, I'm just cold
+Forty somethin' milli' subs or so, I've been told
+I'm in the thick of it, everybody knows (Everybody knows)
+They know me where it snows, I skied in and they froze (Yeah)
+I don't know no nothin' 'bout no ice, I'm just cold
+Forty somethin' milli' subs or so, I've been told (Ooh-ooh)
+
+Woah-oh-oh (Nah-nah-nah-nah, ayy, ayy)
+This is how the story goes (Nah, nah)
+Woah-oh-oh
+I guess this is how the story goes"""
+
 # Library imports
 from vex import *
 
